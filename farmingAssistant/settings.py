a0a72a8a1fee4ce75865eb1fe1c5e57f7e7ae4d8
Shristy singh <105601555+shristy-kumari@users.noreply.github.com>
2022-05-17 11:11:00 -0700

    update

--- farmingAssistant/settings.py
+++ farmingAssistant/settings.py
@@ -91,12 +91,8 @@ WSGI_APPLICATION = 'farmingAssistant.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'farming_db', 
-        'USER': 'postgres', 
-        'PASSWORD': 'shristy',
-        'HOST': '127.0.0.1', 
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
